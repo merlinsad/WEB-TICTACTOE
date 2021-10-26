086dd411e68f44f28f6e268c1c1525af6d5e1b32
Vinicius Vicenzo <vvprockl@outlook.com>
2021-10-26 17:06:40 -0300

    Plataforma Site

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost='Localhost';
+    $dbUser= 'root';
+    $dbPassword='';
+    $dbName='formulario_jogo';
+
+    $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+?>
